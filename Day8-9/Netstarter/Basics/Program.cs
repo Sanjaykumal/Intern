0661ea71a940c83500188d4811922754217396b4
Sanjay <sk5248916@gmail.com>
2024-04-11 22:57:38 +0545

    animalclassanditsinternalfields

--- Day8-9/Netstarter/Basics/Program.cs
+++ Day8-9/Netstarter/Basics/Program.cs
@@ -1,26 +1,39 @@
 ﻿//top level statement
-Student s1 = new();
-Student s2 = new();
-Student s3 = new();
-Student s4 = new();
-Student s5 = new();
-Student s6 = new();
+//Student s1 = new();
+//Student s2 = new();
+//Student s3 = new();
+//Student s4 = new();
+//Student s5 = new();
+//Student s6 = new();
 
-s1.name = "Bishnu";
-s1.address= "Ktm";
-s1.roll = 23;
-s1.dob = new DateTime(1990, 12, 12);
-
-
+//s1.name = "Bishnu";
+//s1.address= "Ktm";
+//s1.roll = 23;
+//s1.dob = new DateTime(1990, 12, 12);
 
 
 
-s1.PrintDetails();
 
-//create  class called "animlas "
+
+//s1.PrintDetails();
+
+//create  class called "animals "
 //define atleast 5 internal fields
 //define a method to print al details of animals in descriptive fornmat
 //in program entry points create atleats two objects of animals
 //initialize the fields for each instance
 //print details in console
 
+
+
+Animal a1 = new();
+
+
+
+a1.genus = "Oryctolagus";
+a1.species = "caniculus";
+a1.lifespan = 12;
+a1.height = 50;
+a1.habitat = "Terrestial";
+
+a1.PrintDetails();
