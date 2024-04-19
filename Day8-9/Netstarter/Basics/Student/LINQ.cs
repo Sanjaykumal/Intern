855dac31acbac6132c1e4cf77d3bb8765be95f26
Sanjay <sk5248916@gmail.com>
2024-04-19 10:37:19 +0545

    linq

--- Day8-9/Netstarter/Basics/Student/LINQ.cs
+++ Day8-9/Netstarter/Basics/Student/LINQ.cs
@@ -6,7 +6,13 @@
 class LINQ
 {
     int [] ages = [23,45,,67,8,,9,12,345,889,76,,34,56,78];
-
+  List<Student> students = [
+        new Student() { name = "Ram", address = "Ktm"},
+        new Student() { name = "Laxman", address = "Dang"},
+        new Student() { name = "Bikendra", address = "Ktm"},
+        new Student() { name = "Sandip", address = "Dang"},
+        new Student() { name = "Chahana", address = "Dhangadhi"},
+    ];
     //fin squares of all the number in collection
 
     public void LearLinq()
@@ -28,5 +34,10 @@ class LINQ
         
         //fin the cubes of all even numbers within age collection
          var cubes = from age in ages select age*age*age where(x=>x%2==0);
+        
+        //find the students who lives in Dang and short the result by name
+        var students = from student in students select student orderby name where address like 'Dang';
+    
+    
     }
 }
