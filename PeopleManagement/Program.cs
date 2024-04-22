a6b15b0afcbe1264c4f811db4231cf0d77fa3d63
Sanjaykumal <117702816+Sanjaykumal@users.noreply.github.com>
2024-04-22 15:27:25 +0545

    Delete PeopleManagement directory

--- PeopleManagement/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string filePath = @"C:\Users\ASUS ZENBOOK\Desktop\Intern\PeopleManagement\People.csv";
-
- CSVParser csvParser = new CSVParser();
- csvParser.Parse(filePath);
- csvParser.PrintNames();
